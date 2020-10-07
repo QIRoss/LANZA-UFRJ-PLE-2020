b740fe89a94610eca86fbab0782316581e60bca6
Lucas de Queiroz <lucasqreis@gmail.com>
2020-10-07 16:07:35 -0300

    recommit because of aula0701.h wrong directory

---
+++ Aulas-Praticas/aula0701.h
@@ -0,0 +1,25 @@
+/*
+Universidade Federal do Rio de Janeiro
+Escola Politecnica
+Departamento de Eletronica e de Computacao
+EEL270 - Computacao II - Turma 2020/4 - PLE-1
+Prof. Marcelo Luiz Drumond Lanza
+Autor: Lucas de Queiroz dos Reis
+Descricao: aula0701.h
+
+$Author$
+$Date$
+$Log$
+*/
+
+#ifndef _AULA0701_
+#define _AULA0701_                  "@(#)aula0701.h $Revision$"
+
+typedef enum{ok} tipoErros;
+
+tipoErros
+MultiplicarMatrizes (float [5][3], float [3][4], float [5][4]);
+
+#endif
+
+/* $RCSfile$ */
