bb8ad0fecf24a8e6f937f643eec2ee1b05398680
Lucas de Queiroz <lucasqreis@gmail.com>
2020-10-25 00:11:29 -0300

    stoping for today

--- Aulas-Praticas/aula0802.c
+++ Aulas-Praticas/aula0802.c
@@ -4,7 +4,7 @@ Departamento de Eletronica e de Computacao
 EEL270 - Computacao II - Turma 2020/4 - PLE-1
 Prof. Marcelo Luiz Drumond Lanza
 Autor: Lucas de Queiroz dos Reis
-Descricao: aula0801.c
+Descricao: aula0802.c
 
 $Author$
 $Date$
